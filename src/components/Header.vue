ac63b46c62332ca6f5c32cc186247ab98c9ff147
Sebastian Petersen <sebkirller@gmail.com>
2019-04-29 14:13:57 +0200

    ?

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-nav collapse navbar-collapse">
+      <router-link class="nav-link nav-item" to="/">Home</router-link>
+      <router-link class="nav-link nav-item" to="/about">About</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
