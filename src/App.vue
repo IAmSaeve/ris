544b1284062971c5725c62fdf05757e5c05c16c7
Sebastian Petersen <sebkirller@gmail.com>
2019-05-02 12:08:43 +0200

    404 page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
   <div id="app">
     <Header></Header>
-    <div class="row mr-0">
-      <div class="col-xs-6 col"></div>
-      <div class="col-xs-6 col-md-8">
-        <router-view></router-view>
-      </div>
-      <div class="col-xs-6 col"></div>
+    <div>
+      <router-view></router-view>
     </div>
   </div>
 </template>
