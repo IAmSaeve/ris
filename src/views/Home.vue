0d30579ed58e142c0473bd3a41c769a4ab8b23bf
Sebastian Petersen <sebkirller@gmail.com>
2019-05-02 14:15:09 +0200

    Placeholder from design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <section class="information">
+      <p>This sections should hold general information</p>
+    </section>
+    <section class="topRated">
+      <p>This sections should show top rated places</p>
+    </section>
+    <section class="bottom">
+      <p>This sections should display some company information and maybe social information</p>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -7,3 +17,24 @@ export default {
   name: "home"
 };
 </script>
+
+<style scoped>
+.information {
+  text-align: center;
+  overflow: hidden;
+  height: 50rem;
+  background: #fff8e1;
+}
+.topRated {
+  text-align: center;
+  overflow: hidden;
+  height: 50rem;
+  background: #767c9f;
+}
+.bottom {
+  text-align: center;
+  overflow: hidden;
+  height: 50rem;
+  background: #34515e;
+}
+</style>
