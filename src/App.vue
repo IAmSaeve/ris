8420da8a2e298e1701305833bfb58950f2ad7e6f
Sebastian Petersen <sebkirller@gmail.com>
2019-05-02 10:28:15 +0200

    Move bootstrap into script

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,15 @@
 </template>
 
 <script>
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 import Header from "@/components/Header";
+
+Vue.use(BootstrapVue);
+
 export default {
   components: {
     Header: Header
