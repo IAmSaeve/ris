b559b9a28f53e1ccea8eacba0c9113e5da767a8c
Sebastian Petersen <sebkirller@gmail.com>
2019-05-07 16:48:42 +0200

    Improve search, fixed conditional render, lint

--- src/views/Institutions.vue
+++ src/views/Institutions.vue
@@ -10,30 +10,42 @@
               name="select-category"
               class="browser-default custom-select m-3"
             >
-            <option value="">Vælg speciale og praktik</option>
+              <option value="">Vælg speciale og praktik</option>
               <option value="Dagtilbud2Praktik">Dagtilbud 2. praktik</option>
-              <option value="Skolefritid2Praktik">Skole fritid 2. praktik</option>
-              <option value="SocialSpecial2Praktik">Social special 2. praktik</option>
+              <option value="Skolefritid2Praktik"
+                >Skole fritid 2. praktik</option
+              >
+              <option value="SocialSpecial2Praktik"
+                >Social special 2. praktik</option
+              >
               <option value="Dagtilbud3Praktik">Dagtilbud 3. praktik</option>
-              <option value="Skolefritid3Praktik">Skole fritid 3. praktik</option>
-              <option value="SocialSpecial3Praktik">Social special 3. praktik</option>
+              <option value="Skolefritid3Praktik"
+                >Skole fritid 3. praktik</option
+              >
+              <option value="SocialSpecial3Praktik"
+                >Social special 3. praktik</option
+              >
             </select>
             <input
               type="search"
               name="search-place"
-              placeholder="Search..."
+              placeholder="Søg... (alle steder)"
               v-model="searchQuery"
               v-on:input="search"
               class="form-control m-3"
               aria-label="Search"
-            >
+            />
           </div>
         </div>
       </div>
     </section>
     <section>
       <div class="container" v-if="searchQuery === ''">
-        <div class="row" v-for="(places, grpIndex) in grpResult" :key="grpIndex">
+        <div
+          class="row"
+          v-for="(places, grpIndex) in grpResult"
+          :key="grpIndex"
+        >
           <div
             class="d-flex flex-column col-lg-4 col-md-12 mb-4 align-items-stretch"
             v-for="(place, index) in places"
@@ -45,9 +57,9 @@
                 <p class="mb-0">{{ place["Praktikstedets adresse"] }}</p>
                 <p class="mb-0">
                   {{
-                  place["Praktikstedets postnummer"] +
-                  " " +
-                  place["Praktikstedets by"]
+                    place["Praktikstedets postnummer"] +
+                      " " +
+                      place["Praktikstedets by"]
                   }}
                 </p>
               </div>
@@ -56,7 +68,11 @@
         </div>
       </div>
       <div class="container" v-else>
-        <div class="row" v-for="(places, grpIndex) in grpResult" :key="grpIndex">
+        <div
+          class="row"
+          v-for="(places, grpIndex) in grpResult"
+          :key="grpIndex"
+        >
           <div
             class="col-lg-4 col-md-12 mb-4 align-items-stretch"
             v-for="(place, index) in places"
@@ -68,9 +84,9 @@
                 <p class="mb-0">{{ place["Praktikstedets adresse"] }}</p>
                 <p class="mb-0">
                   {{
-                  place["Praktikstedets postnummer"] +
-                  " " +
-                  place["Praktikstedets by"]
+                    place["Praktikstedets postnummer"] +
+                      " " +
+                      place["Praktikstedets by"]
                   }}
                 </p>
               </div>
@@ -90,9 +106,9 @@ var Fuse = require("fuse.js");
 var options = {
   shouldSort: true,
   findAllMatches: true,
-  threshold: 0.2,
+  threshold: 0.3,
   location: 0,
-  distance: 10,
+  distance: 100,
   maxPatternLength: 16,
   minMatchCharLength: 3,
   keys: [
@@ -120,13 +136,20 @@ export default {
   },
   methods: {
     search() {
-      // TODO: Search can be made dynamic so that you can query for more then just the address.
-      let fuse = new Fuse(this.allData, options);
-      this.result = fuse.search(this.searchQuery);
+      if (this.searchQuery === "" && this.selectedCategory !== "") {
+        this.result = this.$store.state[this.selectedCategory];
+      } else {
+        this.selectedCategory = "";
+        let fuse = new Fuse(this.allData, options);
+        this.result = fuse.search(this.searchQuery);
+      }
     },
     selectInstitutionData() {
-      if (this.selectedCategory !== "") {
+      this.searchQuery = "";
+      if (this.selectedCategory !== "" && this.searchQuery === "") {
         this.result = this.$store.state[this.selectedCategory];
+      } else {
+        this.result = [];
       }
     }
   },
