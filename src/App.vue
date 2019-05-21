6c093bd35102d0988cddac6e341a86d0afa0b744
Sebastian Petersen <sebkirller@gmail.com>
2019-05-21 17:42:29 +0200

    Trim imports

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+// import "bootstrap-vue/dist/bootstrap-vue.css";
 
 import Header from "@/components/Header";
 
