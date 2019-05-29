f2c35d41263be73a807829ae74e03d0ba844d49f
Sebastian Petersen <sebkirller@gmail.com>
2019-05-29 15:30:06 +0200

    Working mobile navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,28 @@
 <template>
-  <nav v-sticky class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="navbar-nav collapse navbar-collapse">
-      <router-link class="nav-link nav-item" to="/">Home</router-link>
-      <router-link class="nav-link nav-item" to="/about">About</router-link>
-      <router-link class="nav-link nav-item" to="/institutions"
-        >Institutions</router-link
-      >
-    </div>
-  </nav>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <b-button v-b-toggle="'collapse-2'" class="m-1">
+        <span class="navbar-toggler-icon"></span>
+      </b-button>
+
+      <div class="navbar-nav collapse navbar-collapse">
+        <router-link class="nav-link nav-item" to="/">Home</router-link>
+        <router-link class="nav-link nav-item" to="/about">About</router-link>
+        <router-link class="nav-link nav-item" to="/institutions"
+          >Institutions</router-link
+        >
+      </div>
+    </nav>
+    <b-collapse id="collapse-2">
+      <div class="card-group" id="mobileNav">
+        <router-link class="ml-3 mt-2" to="/">Home</router-link>
+        <router-link class="ml-3 mt-2" to="/about">About</router-link>
+        <router-link class="ml-3 mt-2 mb-3" to="/institutions"
+          >Institutions</router-link
+        >
+      </div>
+    </b-collapse>
+  </div>
 </template>
 
 <script>
@@ -26,3 +30,23 @@ export default {
   name: "Header"
 };
 </script>
+
+<style scoped>
+button {
+  display: none;
+}
+
+@media only screen and (max-width: 768px) {
+  button {
+    display: block;
+  }
+}
+
+#collapse-2 {
+  background: #343a40;
+}
+
+#mobileNav a {
+  color: white;
+}
+</style>
