d5a47fe55f7398669e5653cc1fe8eb691d42db1a
Sebastian Petersen <sebkirller@gmail.com>
2019-06-06 20:26:20 +0200

    Migrate to firestore :upside_down_face:

--- src/Store/actions.js
+++ src/Store/actions.js
@@ -7,86 +7,62 @@ import db from "@/components/firebase.js";
 
 const getD2PData = ({ commit }) => {
   let collection = [];
-  db.ref("Dagtilbud2Praktik").on(
-    "value",
-    snapshot => {
-      collection = snapshot.val();
+  db.collection("Dagtilbud2Praktik")
+    .get()
+    .then(querySnapshot => {
+      collection = querySnapshot.docs;
       commit("GET_D2P_DATA", collection);
-    },
-    errorObject => {
-      console.log("The read failed: " + errorObject.code);
-    }
-  );
+    });
 };
 
 const getSF2PData = ({ commit }) => {
   let collection = [];
-  db.ref("Skolefritid2Praktik").on(
-    "value",
-    snapshot => {
-      collection = snapshot.val();
+  db.collection("Skolefritid2Praktik")
+    .get()
+    .then(querySnapshot => {
+      collection = querySnapshot.docs;
       commit("GET_SF2P_DATA", collection);
-    },
-    errorObject => {
-      console.log("The read failed: " + errorObject.code);
-    }
-  );
+    });
 };
 
 const getSS2PData = ({ commit }) => {
   let collection = [];
-  db.ref("SocialSpecial2Praktik").on(
-    "value",
-    snapshot => {
-      collection = snapshot.val();
+  db.collection("SocialSpecial2Praktik")
+    .get()
+    .then(querySnapshot => {
+      collection = querySnapshot.docs;
       commit("GET_SS2P_DATA", collection);
-    },
-    errorObject => {
-      console.log("The read failed: " + errorObject.code);
-    }
-  );
+    });
 };
 
 const getD3PData = ({ commit }) => {
   let collection = [];
-  db.ref("Dagtilbud3Praktik").on(
-    "value",
-    snapshot => {
-      collection = snapshot.val();
+  db.collection("Dagtilbud3Praktik")
+    .get()
+    .then(querySnapshot => {
+      collection = querySnapshot.docs;
       commit("GET_D3P_DATA", collection);
-    },
-    errorObject => {
-      console.log("The read failed: " + errorObject.code);
-    }
-  );
+    });
 };
 
 const getSF3PData = ({ commit }) => {
   let collection = [];
-  db.ref("Skolefritid3Praktik").on(
-    "value",
-    snapshot => {
-      collection = snapshot.val();
+  db.collection("Skolefritid3Praktik")
+    .get()
+    .then(querySnapshot => {
+      collection = querySnapshot.docs;
       commit("GET_SF3P_DATA", collection);
-    },
-    errorObject => {
-      console.log("The read failed: " + errorObject.code);
-    }
-  );
+    });
 };
 
 const getSS3PData = ({ commit }) => {
   let collection = [];
-  db.ref("SocialSpecial3Praktik").on(
-    "value",
-    snapshot => {
-      collection = snapshot.val();
+  db.collection("SocialSpecial3Praktik")
+    .get()
+    .then(querySnapshot => {
+      collection = querySnapshot.docs;
       commit("GET_SS3P_DATA", collection);
-    },
-    errorObject => {
-      console.log("The read failed: " + errorObject.code);
-    }
-  );
+    });
 };
 
 export default {
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,15 +1,12 @@
 import firebase from "firebase/app";
-import "firebase/database";
+import "firebase/firestore";
 
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyA1PIj-7Z0mrJx7fSjppK9E4q2IWF9Km_I",
   authDomain: "rateyourinternship-94023.firebaseapp.com",
-  databaseURL: "https://rateyourinternship-94023.firebaseio.com",
-  projectId: "rateyourinternship-94023",
-  storageBucket: "rateyourinternship-94023.appspot.com",
-  messagingSenderId: "718367514324"
+  projectId: "rateyourinternship-94023"
 };
 const db = firebase.initializeApp(config);
 
-export default db.database();
+export default db.firestore();
