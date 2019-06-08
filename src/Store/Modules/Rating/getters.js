a973f7d0c6390b71c7163a662f5914ad40fc70e5
Sebastian Petersen <sebkirller@gmail.com>
2019-06-08 20:10:06 +0200

    Get avarage rating

--- src/Store/Modules/Rating/getters.js
+++ src/Store/Modules/Rating/getters.js
@@ -1,8 +1,12 @@
 // TODO: Make this useful
-const Ratings = state => {
-  return state.Ratings;
+const getAvarageRating = state => {
+  let avarage = 0;
+  state.Ratings.forEach(rating => {
+    avarage += rating.Rating;
+  });
+  return parseFloat((avarage / state.Ratings.length).toFixed(2));
 };
 
 export default {
-  Ratings
+  getAvarageRating
 };
