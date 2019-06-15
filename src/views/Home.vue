4002737b9a7dad05407527830943454abc749d61
Sebastian Petersen <sebkirller@gmail.com>
2019-06-15 17:55:30 +0200

    Hot damn, så kom der en home page. Tak html5up.net

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
-  <div class="home">
-    <!-- TODO: Complete sections -->
-    <section class="information">
-      <p>This sections should hold general information</p>
+  <div id="page-wrapper">
+    <!-- Two -->
+    <section id="two" class="wrapper alt style2">
+      <section class="spotlight">
+        <div class="image"><img src="../assets/pic01.jpg" alt="" /></div>
+        <div class="content">
+          <h2>
+            Magna primis lobortis<br />
+            sed ullamcorper
+          </h2>
+          <p>
+            Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
+            imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
+          </p>
+        </div>
+      </section>
+      <section class="spotlight">
+        <div class="image"><img src="../assets/pic02.jpg" alt="" /></div>
+        <div class="content">
+          <h2>
+            Tortor dolore feugiat<br />
+            elementum magna
+          </h2>
+          <p>
+            Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
+            imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
+          </p>
+        </div>
+      </section>
+      <section class="spotlight">
+        <div class="image"><img src="../assets/pic03.jpg" alt="" /></div>
+        <div class="content">
+          <h2>
+            Augue eleifend aliquet<br />
+            sed condimentum
+          </h2>
+          <p>
+            Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
+            imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
+          </p>
+        </div>
+      </section>
     </section>
-    <section class="topRated">
-      <p>This sections should show top rated places</p>
-    </section>
-    <section class="bottom">
-      <p>
-        This sections should display some company information and maybe social
-        information
-      </p>
-    </section>
+
+    <!-- Footer -->
+    <footer id="footer">
+      <ul class="icons">
+        <li>
+          <a href="#" class="icon brands fa-twitter"
+            ><span class="label">Twitter</span></a
+          >
+        </li>
+        <li>
+          <a href="#" class="icon brands fa-facebook-f"
+            ><span class="label">Facebook</span></a
+          >
+        </li>
+        <li>
+          <a href="#" class="icon brands fa-instagram"
+            ><span class="label">Instagram</span></a
+          >
+        </li>
+        <li>
+          <a href="#" class="icon brands fa-dribbble"
+            ><span class="label">Dribbble</span></a
+          >
+        </li>
+        <li>
+          <a href="#" class="icon solid fa-envelope"
+            ><span class="label">Email</span></a
+          >
+        </li>
+      </ul>
+      <ul class="copyright">
+        <li>&copy; Sebastian Petersen</li>
+        <li>
+          Kæmpe tak til: <a href="http://html5up.net">HTML5 UP</a> for designet
+        </li>
+      </ul>
+    </footer>
   </div>
 </template>
 
@@ -22,23 +88,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.information {
-  text-align: center;
-  overflow: hidden;
-  height: 50rem;
-  background: #fff8e1;
-}
-.topRated {
-  text-align: center;
-  overflow: hidden;
-  height: 50rem;
-  background: #767c9f;
-}
-.bottom {
-  text-align: center;
-  overflow: hidden;
-  height: 50rem;
-  background: #34515e;
-}
-</style>
+<style scoped src="@/css/main.css"></style>
