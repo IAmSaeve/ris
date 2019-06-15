323c85dec8ede8b8a018417440ac37e6dd7fbbef
Sebastian Petersen <sebkirller@gmail.com>
2019-06-15 17:03:53 +0200

    Ugly ass shit fucking cancer about page!

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,75 @@
 <template>
-  <section class="about">
-    <h1>This is an about page</h1>
-  </section>
+  <div class="container-fluid p-0 m-0" id="about">
+    <div class="row mr-0">
+      <div class="col p-0 ml-5 mr-5">
+        <article>
+          <h1 class="mb-5">
+            Rate your internship
+          </h1>
+          <div class="d-flex justify-content-center mb-4">
+            <img class="img-fluid" src="../assets/happyPreschool.jpg" />
+          </div>
+          <section>
+            <div>
+            <p>
+              Rate your internship, eller bare RIS, er udviklet til danske
+              pædagoger. Projectet er blevet udviklet med henblik på at løse det
+              voksende problem, at flere og flere praktiksteder ikke lever op
+              til de danske standarder og værdier.
+            </p>
+            <p>
+              RIS giver de studerende en mulighed for at komme med en rating på
+              stedet de har været i praktik. På den måde kan de hjælpe andre
+              studerende med at finde det bedste sted.
+            </p>
+            </div>
+          </section>
+        </article>
+      </div>
+    </div>
+    <div class="row mr-0">
+      <div class="col ml-5 mr-5 p-0">
+        <div>
+          <h1 class="mb-sm-5">
+            Kontakt os
+          </h1>
+          <div class="d-sm-flex justify-content-between">
+            <p>Tlf.nr.: <a href="tel:+45 22 71 12 55">22 71 12 55</a></p>
+            <p>
+              Email:
+              <a href="mailto:sebastian@srmail.dk">sebastian@srmail.dk</a>
+            </p>
+            <p>
+              Twitter:
+              <a
+                href="https://twitter.com/sebkirller"
+                target="_blank"
+                rel="noopener noreferrer"
+                >@sebkirller</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+#about {
+  background: #e3dfdb;
+  height: 54.2rem;
+}
+
+img {
+  max-height: 50vh;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
