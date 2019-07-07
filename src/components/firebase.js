a5480ddaa17f18dbe74614d18c26ba454ad02039
Sebastian Petersen <sebkirller@gmail.com>
2019-07-07 15:14:13 +0200

    Update firebase.js
    Database deleted, remove config.

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,11 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyA1PIj-7Z0mrJx7fSjppK9E4q2IWF9Km_I",
-  authDomain: "rateyourinternship-94023.firebaseapp.com",
-  projectId: "rateyourinternship-94023"
-};
+var config = {};
 const db = firebase.initializeApp(config);
 
 export default db.firestore();
